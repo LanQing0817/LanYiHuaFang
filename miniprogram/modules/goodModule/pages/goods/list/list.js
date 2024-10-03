e9df1ac8d29126c4b5f7635622fead6d45336ac3
LanQing0817 <2411681409@qq.com>
2024-10-03 15:36:13 +0800

    商品列表-获取商品列表数据并渲染

--- miniprogram/modules/goodModule/pages/goods/list/list.js
+++ miniprogram/modules/goodModule/pages/goods/list/list.js
@@ -1,12 +1,32 @@
-// pages/goods/list/index.js
+import {reqGoodsList} from '../../../../../api/good'
 
 Page({
-  /**
-   * 页面的初始数据
-   */
   data: {
     goodsList: [], // 商品列表数据
-    isFinish: false // 判断数据是否加载完毕
+    total:0,//数据总条数
+    isFinish: false, // 判断数据是否加载完毕
+    // 接口请求参数
+    requestData:{
+      page:1,//页码
+      limit:10,//每页请求多少条数据
+      category1Id:'',//一级分类id
+      category2Id:''//二级分类id
+    },
+  },
+  // 获取商品列表的数据
+  async getGoodsList(){
+    const {data}=await reqGoodsList(this.data.requestData)
+    console.log('获取商品列表的数据',data)
+    // 将返回的数据赋值给data中的变量
+    this.setData({
+      goodsList:data.records,
+      total:data.total
+    })
+  },
+  onLoad(options){
+    console.log('商品列表----onLoad方法')
+    Object.assign(this.data.requestData,options)
+    // 获取商品列表数据
+    this.getGoodsList()
   }
-
 })
