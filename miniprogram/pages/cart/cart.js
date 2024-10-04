2a9c371af3cfa98fc239dd59c69437396926fa86
LanQing0817 <2411681409@qq.com>
2024-10-04 15:21:59 +0800

    购物车-更新商品的购买状态

--- miniprogram/pages/cart/cart.js
+++ miniprogram/pages/cart/cart.js
@@ -5,7 +5,7 @@ import {
   userStore
 } from '@/stores/userStore'
 import {
-  reqCartList
+  reqCartList, reqUpdateChecked
 } from '@/api/cart'
 ComponentWithStore({
   storeBindings: {
@@ -42,6 +42,25 @@ ComponentWithStore({
         this.setData({
           cartList,
           emptyDes: cartList === 0 && '还没有添加商品，快去添加吧'
+        })
+      }
+    },
+    // 切换商品的选中状态
+    async updateChecked(event) {
+      // 获取最新的选中状态
+      const { detail } = event
+      // 获取商品的索引和 id
+      const { id, index } = event.target.dataset
+      // 将最新的状态格式化成后端所需要的数据格式
+      const isChecked = detail ? 1 : 0
+
+      // 调用接口，传入参数，更新商品的状态
+      const res = await reqUpdateChecked(id, isChecked)
+
+      // 如果数据更新成功，需要将本地的数据一同改变
+      if (res.code === 200) {
+        this.setData({
+          [`cartList[${index}].isChecked`]: isChecked
         })
       }
     },
