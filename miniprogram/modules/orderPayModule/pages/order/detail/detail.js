f51c8cd8f7beb3197d1e2a55bf808f64e3d70441
LanQing0817 <2411681409@qq.com>
2024-10-04 19:30:54 +0800

    商品结算-收集送达时间

--- miniprogram/modules/orderPayModule/pages/order/detail/detail.js
+++ miniprogram/modules/orderPayModule/pages/order/detail/detail.js
@@ -1,7 +1,10 @@
 import {
-  reqOrderAddress
+  reqOrderAddress,
+  reqOrderInfo,
+  reqBuyNowGoods
 } from '@/api/orderpay'
-
+import { formatTime } from '../../../../../utils/formatTime.js'
+const app = getApp()
 Page({
   data: {
     buyName: '', // 订购人姓名
@@ -11,7 +14,8 @@ Page({
     show: false, // 期望送达日期弹框
     minDate: new Date().getTime(),
     currentDate: new Date().getTime(),
-    orderAddress: {} //收货地址
+    orderAddress: {}, //收货地址
+    orderInfo: {}, // 订单商品详情
   },
 
   // 选择期望送达日期
@@ -23,8 +27,13 @@ Page({
 
   // 期望送达日期确定按钮
   onConfirmTimerPicker(event) {
+    // 使用 new Date 将时间戳转换成 JS 中的日期对象
+    const time = formatTime(new Date(event.detail))
+
+    // 将转换以后的时间赋值给送到时间
     this.setData({
-      show: false
+      show: false,
+      deliveryDate: time
     })
   },
 
@@ -67,9 +76,41 @@ Page({
       orderAddress
     })
   },
+  // 获取订单详情
+  async getOrderInfo() {
+    const {
+      goodsId,
+      blessing
+    } = this.data
+    //判断是否存在商品id
+    // 如果存在调用立即购买商品详情的接口
+    //不存在调用获取订单详情数据接口
+    const {
+      data: orderInfo
+    } = goodsId ? await reqBuyNowGoods({
+      goodsId,
+      blessing
+    }) : await reqOrderInfo()
 
+    // 判断是否存在祝福语
+    // 如果需要购买多个商品，挑选第一个填写了祝福语的商品进行赋值
+    const orderGoods = orderInfo.cartVoList.find((item) => item.blessing !== '')
+
+    this.setData({
+      orderInfo,
+      blessing: orderGoods && orderGoods.blessing
+    })
+  },
   // 页面展示时触发的钩子函数
   onShow() {
     this.getAddress()
-  }
+    // 获取订单结算页面的商品信息
+    this.getOrderInfo()
+  },
+  // 接收立即购买传递的参数
+  onLoad(options) {
+    this.setData({
+      ...options
+    })
+  },
 })
